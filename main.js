f513b3d59056d90ce68edc7fa05e166da835d0f0
qz713274422$ <igor.kravitz@yahoo.com>
2020-12-13 18:42:38 +0200

    1

--- main.js
+++ main.js
@@ -2,25 +2,25 @@
 const fortune = require('./lib/fortune.js');
 const weather = require('./lib/weather');
 const express = require('express');
+const bodyParser = require('body-parser');
+const express_handlebars_sections = require('express-handlebars-sections');
+const formidable = require('formidable');
 const app = express();
-var express_handlebars_sections = require('express-handlebars-sections');
 // Установка механизма представления handlebars
 var handlebars = require('express-handlebars')
     .create({
         defaultLayout: 'main',
         extname: 'hbs',
         helpers: {
-            section: (name, options) => {
-                if (!this._sections) {
-                    this._sections = {};
-                }
+            section: function (name, options) {
+                if (!this._sections) this._sections = {};
                 this._sections[name] = options.fn(this);
                 return null;
             }
         }
-            // layoutsDir: path.join(__dirname, "views/layouts"),
-            // partialsDir: path.join(__dirname, "views/partials"),
-        });
+        // layoutsDir: path.join(__dirname, "views/layouts"),
+        // partialsDir: path.join(__dirname, "views/partials"),
+    });
 app.engine('hbs', handlebars.engine);
 express_handlebars_sections(handlebars);
 // app.set('views', __dirname + '/views/');
@@ -47,14 +47,53 @@ app.get('/about', function (req, res) {
         pageTestScript: '/qa/tests-about.js'
     });
 });
-app.get('/jquery-test', function(req, res){
-	res.render('jquery-test');
+app.get('/jquery-test', function (req, res) {
+    res.render('jquery-test');
 });
 app.get('/tours/hood-river', function (req, res) {
     res.render('tours/hood-river');
 });
+app.get('/tours/oregon-coast', function (req, res) {
+    res.render('tours/oregon-coast');
+});
 app.get('/tours/request-group-rate', function (req, res) {
     res.render('tours/request-group-rate');
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/newsletter', function (req, res) {
+    // мы изучим CSRF позже... сейчас мы лишь    
+    // заполняем фиктивное значение
+    res.render('newsletter', { csrf: 'CSRFtokengoeshere' });
+});
+app.post('/process', function (req, res) {
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+});
+
+app.get('/thank-you', function (req, res) {
+    res.render('thank-you');
+});
+app.get('/contest/vacation-photo', function (req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(), month: now.getMonth()
+    });
+});
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if (err)
+            return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 // Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function (req, res, next) {
--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -3,44 +3,67 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var Browser = require('zombie'),
-        assert = require('chai').assert;
+var Browser = require('zombie'), assert = require('chai').assert;
+// Browser.localhost('localhost', 3000);
 var browser;
 suite('Межстраничные тесты', function () {
     setup(function () {
         browser = new Browser();
+        // this.timeout = 5000;
     });
-    test('запрос расценок для групп со страницы туров по реке Худ'
-            + 'должен заполнять поле реферера', function (done) {
-                var referrer = 'http://localhost:3000/tours/hood-river';
-                browser.visit(referrer, function () {
-                    browser.clickLink('.requestGroupRate', function () {
-                        assert(browser.field('referrer').value
-                                === referrer);
-                        done();
-                    });
-                });
-            });
-    test('запрос расценок для групп со страницы туров '
-            + 'пансионата "Орегон Коуст" должен '
-            + 'заполнять поле реферера', function (done) {
-                var referrer = 'http://localhost:3000/tours/oregon-coast';
-                browser.visit(referrer, function () {
-                    browser.clickLink('.requestGroupRate', function () {
-                        assert(browser.field('referrer').value
-                                === referrer);
-                        done();
-                    });
-                });
-            });
-    test('посещение страницы "Запрос цены для групп" напрямую '
-            + 'должен приводить к пустому полю реферера', function (done) {
-                browser.visit('http://localhost:3000/tours/request-group-rate',
-                        function () {
-                            assert(browser.field('referrer').value === '');
-                            done();
-                        });
-            });
+    test('запрос расценок для групп со страницы туров по реке Худ ' +
+        'должен заполнять поле реферера', async function () {
+            let referrer = 'http://localhost:3000/tours/hood-river';
+            await browser.visit(referrer);
+            await browser.clickLink('.requestGroupRate');
+            // assert(browser.field('email').value === '777@777.com');
+            assert(browser.field('referrer').value === referrer);
+            // console.log(browser.resources[0].request.headers._headers);    
+            // assert(browser.resources[0].request.headers._headers[0][1] === referrer);       
+        });
+    // test('запрос расценок для групп со страницы туров по реке Худ' +
+    //     'должен заполнять поле реферера (callback)', function (done) {
+    //         var referrer = 'http://localhost:3000/tours/hood-river';
+    //         browser.visit(referrer, function () {
+    //             browser.clickLink('.requestGroupRate', function () {
+    //                 assert(browser.field('referrer').value === referrer);
+    //                 setTimeout(done, this.timeout);
+    //             });
+    //         });
+    //     });
+    test('запрос расценок для групп со страницы туров ' +
+        'пансионата "Орегон Коуст" должен ' +
+        'заполнять поле реферера', async function () {
+            let referrer = 'http://localhost:3000/tours/oregon-coast';
+            await browser.visit(referrer);
+            await browser.clickLink('.requestGroupRate');
+            assert(browser.field('referrer').value === referrer);
+            // assert(browser.field('email').value === '777@777.com');
+            // assert(browser.resources[0].request.headers._headers[0][1] === referrer);  
+        });
+    // test('запрос расценок для групп со страницы туров ' +
+    //     'пансионата "Орегон Коуст" должен ' +
+    //     'заполнять поле реферера', function (done) {
+    //         var referrer = 'http://localhost:3000/tours/oregon-coast';
+    //         browser.visit(referrer, function () {
+    //             browser.clickLink('.requestGroupRate', function () {
+    //                 assert(browser.field('referrer').value === referrer);
+    //                 setTimeout(done, this.timeout);
+    //             });
+    //         });
+    //     });
+    test('посещение страницы "Запрос цены для групп" напрямую ' +
+        'должен приводить к пустому полю реферера', async function () {
+            await browser.visit('http://localhost:3000/tours/request-group-rate');
+            assert(browser.field('referrer').value === '');
+        });
+    // test('посещение страницы "Запрос цены для групп" напрямую ' +
+    //     'должен приводить к пустому полю реферера', function (done) {
+    //         browser.visit('http://localhost:3000/tours/request-group-rate', function () {
+    //             assert(browser.field('referrer').value === '');
+    //             setTimeout(done, this.timeout);
+    //         });
+    //     });
 });
 
 
